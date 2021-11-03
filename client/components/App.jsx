7650a8221f3da0c3ed7e03646a4aed2befe42092
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-03 19:21:29 +1300

    set up files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-function App (props) {
+
+const App = () => {
 
   return (
     <>
       <div className='app'>
-        <h1>Welcome to our music database</h1>
       </div>
     </>
   )
 }
 
-export default connect(mapStateToProps)(App)
+export default App
--- client/index.js
+++ client/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+      <App />,
     document.getElementById('app')
   )
 })
