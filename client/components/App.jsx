3e6bdd73b9607d1409e692efbc6575732140c403
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-05 18:20:35 +1300

    created Header component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+// Components
+import Header from './Header'
+
 
 const App = () => {
 
   return (
     <>
       <div className='app'>
+        <Header />
       </div>
     </>
   )
