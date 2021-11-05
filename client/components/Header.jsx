742934e49653b3521dfc871efcacef711cabfdff
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-05 21:33:03 +1300

    styling changes

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,19 +3,28 @@ import '../styles/Header.css'
 
 const Header = () => {
 
-
    return(
       <div className='header-container'>
          <div className='header-title-container'>
             <h1>Taine Rasmussen</h1>
          </div>
          <div className='header-item-container'>
-            <h2>Home</h2>
-            <h2>Portairt</h2>
-            <h2>Street</h2>
+            <ul>
+               <li>Home</li>
+               <li>Portrait</li>
+               <li>Street</li>
+            </ul>
          </div>
       </div>
    )
 }
 
 export default Header
+
+// Paralax scrolling???
+
+// Portfolio idea:
+   // Project section: 
+      // More contained styling 
+      // Use Parallax scrolling
+      // Card positions alternate from each side of the screen
