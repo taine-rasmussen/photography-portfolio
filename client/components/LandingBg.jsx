add6b8952e0a9d66372f68513bd0a82dc6825e73
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-09 17:23:19 +1300

    removed landingBg

--- client/components/LandingBg.jsx
+++ client/components/LandingBg.jsx
@@ -4,29 +4,9 @@ import ParallaxMousemove from 'react-parallax-mousemove'
 
 const LandingBg = () => {
                      
-   const imageExample = <img src="/Images/profile.jpeg" alt="example"/>
-   const imageArr = [imageExample, imageExample, imageExample]         
-   
-
-
    return (
       <div className='landing-bg-container'>  
-         {imageArr.map((img, index) => {
-            return(
-               <div key={index}>
-                  <ParallaxMousemove containerStyle={{ position: 'relative'}} fullHeight={true}>
-                      <ParallaxMousemove.Layer layerStyle={{position: 'absolute', height: '100%',}}
-                        config={{
-                           xFactor: 0.1,
-                           yFactor: 0.1,
-                           springSettings: { stiffness: 50, damping: 10 }
-                        }}>
-                     {img}
-                  </ParallaxMousemove.Layer>
-                  </ParallaxMousemove>
-               </div>
-            )
-         })}
+   
       </div>
    )
 }
