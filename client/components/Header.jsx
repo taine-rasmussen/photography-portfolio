7eb940a72df1f6d69e7c0ca07bfb8f7915e5467f
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-09 18:04:18 +1300

    rebuilt header component

---
+++ client/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Header.css'
+
+const Header = () => {
+   return (
+      <div className="header-container">
+         <div className="header-title-container">
+            <h1>Taine Rasmussen</h1>
+         </div>
+         <div className="header-item-container">
+            <ul>
+               <Link to=''>
+                  <li>Home</li>
+               </Link>
+               <Link to='portrait'>
+                  <li>Portrait</li>
+               </Link>
+               <Link to='street'>
+                  <li>Street</li>
+               </Link>
+            </ul>
+         </div>
+      </div>
+   )
+}
+
+export default Header
--- client/components/Street.jsx
+++ client/components/Street.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 // components
+import Header from './Header'
 
 
 const Street = () => {
