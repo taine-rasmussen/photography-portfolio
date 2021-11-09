ae5129d5bcbddb3fa43ee09f08129424e8031c6e
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-11-09 17:57:06 +1300

    broke everything??? fixed it by deleting a whole component lol

--- client/components/Portrait.jsx
+++ client/components/Portrait.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 import '../styles/Portrait.css'
 
 // Components
-import Header from './header'
 
 const Portrait = () => {
 
    const image = <img src="/Images/profile.jpeg" alt="example"/>
    const portraitImages = [image, image, image, image, image, image, image, image];
 
-
-
    return (
       <>
-         <Header />
             <div className="portrait-container">
                {portraitImages.map((img, index) => {
                   return(
@@ -22,7 +18,7 @@ const Portrait = () => {
                      </div>
                   )
                })}
-            </div>
+           </div>
       </>
    )
 }
