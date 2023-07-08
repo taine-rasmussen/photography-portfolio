c26a712ec2f5aef700c3f1570ae190d6112daab5
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-07-08 19:09:39 +0100

    font theme

--- src/themeConfig.ts
+++ src/themeConfig.ts
@@ -8,9 +8,25 @@ const palette: PaletteOptions = {
   primary: {
     main: '#000000',
   },
+  secondary: {
+    main: '#fff'
+  }
 };
 
 const typography: TypographyOptions = {
+  fontFamily: 'Playfair Display"',
+    fontSize: 16,
+    h1: {
+      fontSize: '3rem',
+      lineHeight: 1.2,
+      letterSpacing: '-0.02em',
+    },
+    h2: {
+      fontSize: '2.25rem',
+      fontWeight: 'bold',
+      lineHeight: 1.2,
+      letterSpacing: '-0.02em',
+    },
 };
 
 const themeOptions: ThemeOptions = {
